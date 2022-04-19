2a1c34aa3ed55b0806c04332c86990d7d118e3ab
petru.stepan <petru.stepan@D20-05>
2022-04-19 11:33:09 +0200

    kostky

--- Kostky/MainWindow.xaml.cs
+++ Kostky/MainWindow.xaml.cs
@@ -20,11 +20,42 @@ namespace Kostky
     /// </summary>
     public partial class MainWindow : Window
     {
-        //Kostka[] kostky = new Kostka[6];
-        Kostka[] kostky = new Kostka[]
+        //Kostka[] kostky = new Kostka[];
+        Kostka[] kostky = new Kostka[]{
+        new Kostka(),
+        new Kostka(),
+        new Kostka(),
+        new Kostka(),
+        new Kostka(),
+        new Kostka()
+        };
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void ZobrazKostky()
+        {
+            Kostka0.Content = kostky[0].Hodnota;
+            Kostka1.Content = kostky[1].Hodnota;
+            Kostka2.Content = kostky[2].Hodnota;
+            Kostka3.Content = kostky[3].Hodnota;
+            Kostka4.Content = kostky[4].Hodnota;
+            Kostka5.Content = kostky[5].Hodnota;
+        }
+
+        private void btnGandalf_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var kostka in kostky)
+            {
+                kostka.Hod();
+            }
+            ZobrazKostky();
+        }
+        private int SpocitejBody()
+        {
+            int body = 0;
+            Dictionary<int, int> pocty = new Dictionary<int, int>();
+        }
+        return body;
     }
 }
